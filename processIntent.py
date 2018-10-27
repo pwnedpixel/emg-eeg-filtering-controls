35c7487296ec15336f53ccc582ceed93203e1455
Andy K <andy@akeech.com>
2018-10-27 14:15:59 -0400

    code cleanup, removed unused code, added more comments

--- processIntent.py
+++ processIntent.py
@@ -1,6 +1,9 @@
 # given a filtered EMG signal, determines whether or not the brace should move
 def processIntent(filteredSignal):
-    if (sum(filteredSignal)/len(filteredSignal) > 0.25):
+    moveThres = 0.25
+
+    # if the mean signal is above moveThres (samples can be 0-1), the brace should move
+    if (sum(filteredSignal)/len(filteredSignal) > moveThres):
         return 1
     else:
         return 0
