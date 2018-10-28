24e1036d9f7313a514e0db43a38a515d93b3d3d3
Andy K <andy@akeech.com>
2018-10-27 20:09:20 -0400

    updated performance test

---
+++ performanceTesting.py
@@ -0,0 +1,11 @@
+import numpy as np
+import timeit
+from filterEMG import filterEMG
+import random
+import timeit
+from signalGenerator import *
+
+
+print timeit.timeit('filterEMG(time, emg)','from signalGenerator import getSampleTwo; from filterEMG import filterEMG; emg, time = getSampleTwo()', number=1000)
+print timeit.timeit('filterEMG(time, emg)','from signalGenerator import getSampleTwo; from filterEMG import filterEMG; emg, time = getSampleTwo()', number=1000)
+print timeit.timeit('filterEMG(time, emg)','from signalGenerator import getSampleTwo; from filterEMG import filterEMG; emg, time = getSampleTwo()', number=1000)
