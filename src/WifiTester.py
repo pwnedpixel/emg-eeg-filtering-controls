0f529bebe82b52b9cb0db017a1103e6c3a6f7d85
Andy K <andy@akeech.com>
2019-03-13 18:03:19 -0400

    started working on startup script

--- src/WifiTester.py
+++ src/WifiTester.py
@@ -3,14 +3,40 @@ import json
 import struct
 import binascii
 import datetime
+import requests
 
 HOST = '192.168.4.2'  # Standard loopback interface address (localhost)
 PORT = 145       # Port to listen on (non-privileged ports are > 1023)
+
+# Setup the board
+URL = "http://192.168.4.1"
+
+commandBody = {'command':'{'}
+r = requests.post(url = URL+"/command", data=commandBody)
+print r.text
+commandBody = {'command':'~6'}
+r = requests.post(url = URL+"/command", data=commandBody)
+print r.text
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(5)
 conn, addr = s.accept()
+
+# false = False
+# true=True
+
+# commandBody = {
+#   'delimiter': false,
+#   'ip': "192.168.4.2",
+#   'latency': 5000,
+#   'output': 'raw",
+#   'port': 145,
+#   'sample_numbers': false,
+#   'timestamps': true
+# }
+# r = requests.get(url = URL+"/command", body = commandBody)
+# print r.json()
 
 i = 0
 while i < 1600:
